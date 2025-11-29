cd6c2d8f1e8e1d54def12de1af5ae296d91379ff
Rucha Nimbalkar <rucha.nimbalkar90@gmail.com>
2025-11-28 19:49:40 -0800

    Delete server/src/config.js

--- server/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  databaseUrl:
-    "postgresql://neondb_owner:npg_nOJy5LS6IuGR@ep-fancy-wildflower-af5behfk-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-};
-
-export default config;
